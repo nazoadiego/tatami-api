a9a2d7d3dba2253216088ab579d9c463dfc1df7b
diegomarrufo <dmarrufo@ucm.es>
2022-05-12 17:08:04 +0200

    resolved cors using rack-cors gem. Allowed my next app to fetch from here

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,15 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3001'
+    resource '/tasks',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '/projects',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
